43f98ba9f85759cb0827a4b61fb588b2c33c024d
V00D001337 <45878232+V00D001337@users.noreply.github.com>
2021-05-27 00:42:20 +0200

    RegisterContainer + RegisterForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import { LoginContainer } from './login/containers/LoginContainer';
+import { RegisterContainer } from './register/container/RegisterContainer';
 
 function App() {
   return (
-    <LoginContainer/>
+    <div>
+      <LoginContainer/>
+      {/*<RegisterContainer/>*/}
+    </div>
   );
 }
 
--- src/register/components/RegisterForm.js
+++ src/register/components/RegisterForm.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                    <label htmlFor="nameInput">Name</label>
+                    <input {...register("name")} className="form-control" type="text" id="nameInput" />
+                </div>
                 <div className="form-group">
                     <label htmlFor="emailInput">Email address</label>
                     <input {...register("email")} className="form-control" type="text" id="emailInput" />
@@ -16,9 +20,8 @@ export const LoginForm = () => {
                     <label htmlFor="passwordInput">Password</label>
                     <input {...register("password")} className="form-control" type="password" id="passwordInput" />
                 </div>
-                <button type="submit" className="btn btn-primary">Login</button>
+                <button type="submit" className="btn btn-primary">Register</button>
             </form>
-            <button className="btn btn-success">Sign up</button>
         </div>
     )
 }
